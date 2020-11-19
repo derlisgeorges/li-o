4f4b8f1e1759fa0da3e09ff639d19da012f6a2a5
DEV <dev@info.local>
2020-11-19 16:59:19 -0300

    19

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Arrays
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string [] produtos = {"maçã", "batata", "cenoura"};
+            double[] preços = {1.1, 2.3, 4.5};
+            int[] idades = {16, 17, 18};
+
+            string[] nomesAlunos = new string [5];
+
+            // Console.WriteLine(produtos[1]);
+
+            // Console.WriteLine("Trocar batata por qual item?");
+
+            // produtos[1] = Console.ReadLine();
+
+            // Console.WriteLine(produtos[1]);
+
+            // Console.WriteLine(produtos.Length);
+
+            for (int i = 0; i < produtos.Length; i++)
+            {
+                // Console.WriteLine(i);
+                Console.WriteLine(produtos[i]);
+            }
+
+
+        }
+    }
+}
